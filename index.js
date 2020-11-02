52a8da1f2370e1267b939f1926f09f115ce5de19
Jay Grant <jaykbgrant@gmail.com>
2020-11-02 15:42:17 +0000

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var questions = [
   {
-    Question:"Question 1,
-    Answers:["answer 1","answer 2","answer 3", "answer 4"],
-    Correct:["answer 1","answer 2","answer 3", "answer 4"],
-    Catergory:1
+    question:"Question 1",
+    answers:["answer 1","answer 2","answer 3","answer 4"],
+    correct:["answer 1","answer 2","answer 3","answer 4"],
+    catergory:"Sports",
   },
 
-  
+console.log(questions[0])  
 ]
