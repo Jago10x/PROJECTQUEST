872cdf269de322b6e206c0edfb3ad3882625a0b4
Jay Grant <jaykbgrant@gmail.com>
2020-11-02 15:36:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var questions = [
+  {
+    Question:"Question 1,
+    Answers:["answer 1","answer 2","answer 3", "answer 4"],
+    Correct:["answer 1","answer 2","answer 3", "answer 4"],
+    Catergory:1
+  },
+
+  
+]
